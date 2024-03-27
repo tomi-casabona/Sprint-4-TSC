ed3ff1d3a41dc7432554a11d925a49e75cb85d45
tomi-casabona <tomas.casabona@gmail.com>
2024-03-27 12:37:17 +0100

    Exercise 2 - made a scoring system with buttons and made the page look better

--- index.js
+++ index.js
@@ -8,20 +8,99 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let joke = document.getElementsByClassName("joke")[0];
-let jokeBtn = document.getElementsByClassName("get-joke")[0];
-let API = 'https://icanhazdadjoke.com/';
+let jokeElement = document.getElementById("joke"); // joke paragraph element
+let jokeBtn = document.getElementById("next"); // next joke button
+let API = 'https://icanhazdadjoke.com/'; // API endpoint
+let currentReport; // jokeReport variable
+let reportAcudits = []; // Array of joke reports
+let btnScore1 = document.getElementById("score1"); // button of "boring"
+let btnScore2 = document.getElementById("score2"); // button of "average"
+let btnScore3 = document.getElementById("score3"); // button of "very nice"
 function addJokeToParagraph() {
     return __awaiter(this, void 0, void 0, function* () {
-        let res = yield fetch(API, {
-            headers: {
-                'accept': 'application/json',
+        try {
+            // try to make the request to the API and get the data
+            let res = yield fetch(API, {
+                headers: {
+                    'accept': 'application/json',
+                }
+            });
+            // Verify if the response is ok, if it isn't then throw an error
+            if (!res.ok) {
+                throw new Error('Error al obtener la respuesta de la API');
             }
-        });
-        let data = yield res.json();
-        console.log(data);
-        joke.innerHTML = data.joke;
+            // Pick the data from the json formant and assign to 'data' variable
+            let data = yield res.json();
+            // If jokeElement exists then assign the new joke from data.joke
+            if (jokeElement) {
+                jokeElement.innerHTML = data.joke;
+            }
+            else {
+                throw new Error('JokeElement is null');
+            }
+        }
+        catch (e) {
+            // Catch all exceptions from API request
+            if (e instanceof Error) {
+                throw new Error(e.message);
+            }
+            else {
+                console.log('error: ' + e);
+            }
+        }
     });
 }
+//Add the first joke in the html
 addJokeToParagraph();
-jokeBtn.addEventListener('click', addJokeToParagraph);
+//Add event listener to 'Next' button for execute addJokeToParagraph function, if jokeBtn exists
+if (jokeBtn) {
+    jokeBtn.addEventListener('click', () => nextJoke());
+}
+else {
+    throw new Error("Next button is not found");
+}
+//Add event listener for scoring buttons
+if (btnScore1) {
+    btnScore1.addEventListener('click', () => createReport(1));
+}
+if (btnScore2) {
+    btnScore2.addEventListener('click', () => createReport(2));
+}
+if (btnScore3) {
+    btnScore3.addEventListener('click', () => createReport(3));
+}
+// createReport function create an object type JokeReport where the client leave the joke's score (1|2|3) and store it with the joke and the date
+function createReport(num) {
+    if (jokeElement) {
+        let report = {
+            joke: jokeElement.textContent,
+            score: num,
+            date: new Date().toISOString()
+        };
+        currentReport = report;
+        return report;
+    }
+    else {
+        throw new Error("JokeElement is not found");
+    }
+}
+// addReport function receive a report parameter and save it in reportAcudits array when nextJoke button is clicked
+function addReport() {
+    if (currentReport) {
+        reportAcudits.push(currentReport); // add report to reportAcudits array
+        currentReport = undefined; //reset variable
+    }
+    else {
+        console.log("last joke is not scored");
+    }
+}
+// show reportAcudits array
+function showJokesReport() {
+    console.log(reportAcudits);
+}
+// nextJoke function call addReport function and call addJokeReport function;
+function nextJoke() {
+    addReport();
+    addJokeToParagraph();
+    showJokesReport();
+}
--- index.ts
+++ index.ts
@@ -1,24 +1,113 @@
-let joke : Element  = document.getElementsByClassName("joke")[0];
-let jokeBtn : Element = document.getElementsByClassName("get-joke")[0];
+let jokeElement = document.getElementById("joke"); // joke paragraph element
+let jokeBtn = document.getElementById("next"); // next joke button
+let API: string = 'https://icanhazdadjoke.com/'; // API endpoint
+let currentReport : JokeReport | undefined; // jokeReport variable
+let reportAcudits: JokeReport[] = []; // Array of joke reports
+let btnScore1 = document.getElementById("score1");// button of "boring"
+let btnScore2 = document.getElementById("score2");// button of "average"
+let btnScore3 = document.getElementById("score3");// button of "very nice"
 
-let API : string = 'https://icanhazdadjoke.com/';
-
+/*
+Type JokeReport 
+@Param joke : string
+@Param score : 1|2|3
+@param date : string (date in format ISO)
+*/
+type JokeReport = {
+    joke: string | null,
+    score: 1 | 2 | 3,
+    date: string
+}
 
 async function addJokeToParagraph() {
+    try {
+        // try to make the request to the API and get the data
+        let res = await fetch(API, {
+            headers: {
+                'accept': 'application/json',
+            }
+        });
 
-    let res =  await fetch(API, {
-        headers: {
-            'accept': 'application/json',
+        // Verify if the response is ok, if it isn't then throw an error
+        if (!res.ok) {
+            throw new Error('Error al obtener la respuesta de la API');
         }
-    });
 
-    let data =  await res.json();
+        // Pick the data from the json formant and assign to 'data' variable
+        let data = await res.json();
 
-    console.log(data);
-    joke.innerHTML = data.joke ;
+        // If jokeElement exists then assign the new joke from data.joke
+        if (jokeElement) {
+            jokeElement.innerHTML = data.joke;
+        } else {
+            throw new Error('JokeElement is null')
+        }
+        
+    } catch (e) {
+        // Catch all exceptions from API request
+        if(e instanceof Error){
+            throw new Error(e.message);
+            
+        }else{
+            console.log('error: ' + e);            
+        }
+    }
 }
+//Add the first joke in the html
+addJokeToParagraph(); 
+
+//Add event listener to 'Next' button for execute addJokeToParagraph function, if jokeBtn exists
+if(jokeBtn){
+jokeBtn.addEventListener('click', ()=>nextJoke());
+}else{
+    throw new Error("Next button is not found");   
+}
+
+//Add event listener for scoring buttons
+
+if(btnScore1){
+    btnScore1.addEventListener('click', ()=> createReport(1));
+}
+if(btnScore2){
+    btnScore2.addEventListener('click', ()=>createReport(2));
+}
+if(btnScore3){
+    btnScore3.addEventListener('click', ()=>createReport(3));
+}
+
+
+// createReport function create an object type JokeReport where the client leave the joke's score (1|2|3) and store it with the joke and the date
+function createReport(num: 1 | 2 | 3) : JokeReport {
+if(jokeElement){
+    let report: JokeReport = {
+        joke: jokeElement.textContent,
+        score: num,
+        date: new Date().toISOString()
+    }
+    currentReport = report;
+    return report;
+    
+}else{
+    throw new Error("JokeElement is not found");
+}
+}
+
+// addReport function receive a report parameter and save it in reportAcudits array when nextJoke button is clicked
+function addReport(){        
+    if(currentReport){
+    reportAcudits.push(currentReport); // add report to reportAcudits array
+    currentReport = undefined; //reset variable
+}else{console.log("last joke is not scored")}
+}
+
+// show reportAcudits array
+function showJokesReport(){
+    console.log(reportAcudits)
+}
+
+// nextJoke function call addReport function and call addJokeReport function;
+function nextJoke(){
+addReport();   
 addJokeToParagraph();
-
-jokeBtn.addEventListener('click', addJokeToParagraph);
-
-
+showJokesReport();
+}
